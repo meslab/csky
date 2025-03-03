16cd2c1adb6ccd299956ea3cd71ee32a7f4339b8
Anton Sidorov <antosid@gmail.com>
2025-03-03 18:19:01 +0000

    -

--- include/processor.h
+++ include/processor.h
@@ -2,5 +2,6 @@
 #define PROCESSOR_H
 
 void *data_processor_thread(void *arg);
+void strip_chars(char *str);
 
 #endif // PROCESSOR_H
--- src/processor.c
+++ src/processor.c
@@ -1,29 +1,36 @@
 #include "../include/processor.h"
+#include "../include/ads_b.h"
 #include "../include/ring_buffer.h"
 #include "../include/utils.h"
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Data processing thread: Reads from ring buffer, processes hex data
  */
 void *data_processor_thread(void *arg) {
-    ring_buffer_t *rb = (ring_buffer_t *)arg;
-    uint32_t processed_data[8];
-    int count;
-  
-    while (1) {
-      char line[MAX_LINE_LENGTH + 1];
-      if (ring_buffer_get(rb, line) == 0) {
-        hex_to_uint32(line, processed_data, &count);
-        printf("Processed data: ");
-        for (int i = 0; i < count; i++) {
-          printf("%X ", processed_data[i]);
-        }
-        printf("\n");
-      }
+  ring_buffer_t *rb = (ring_buffer_t *)arg;
+
+  while (1) {
+    char line[MAX_LINE_LENGTH + 1];
+    if (ring_buffer_get(rb, line) == 0) {
+      strip_chars(line);
+      printf("%s\n", line);
+      process_adsb(line);
     }
-    return NULL;
   }
-  
+  return NULL;
+}
+
+void strip_chars(char *str) {
+  int i, j = 0;
+  int len = strlen(str);
+
+  for (i = 0; i < len; i++) {
+    if (str[i] != '@' && str[i] != ';') {
+      str[j++] = str[i];
+    }
+  }
+  str[j] = '\0';
+}
